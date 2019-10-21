11ad6c86b98c4d8f465fc31a28e54f2d8940cf5b
Dinuka Jayalath <dinukasandaruwan.ds@gmail.com>
2019-10-21 23:21:07 +0530

    Home page timer date change & Contact Mailto Update
    Team Name:- Open5
    
    Home page timer date updated and in cintact.html mailto added to send email when click on the Email address.

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -225,7 +225,7 @@
     // :: 13.0 Countdown Active Code
     // ****************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2019/08/29", function (event) {
+        $("#clock").countdown("2019/12/29", function (event) {
             $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }

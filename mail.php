e6458a9e6ade84c5f011d0ba04c5de1294555abd
AnukiAlwis <anu.alwis1998@gmail.com>
2019-10-22 19:33:36 +0530

    added a new php file for contacts form

---
+++ mail.php
@@ -0,0 +1,45 @@
+﻿<?php
+ 
+if($_POST) {
+    $name = "";
+    $email = "";
+    $email = "";
+    $subject = "";
+    $message = "";
+     
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
+     
+    if(isset($_POST['email'])) {
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    }
+     
+   
+     if(isset($_POST['subject'])) {
+        $message = htmlspecialchars($_POST['message']);
+    }
+     
+   
+     
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+     
+    if(mail($recipient, $email_title, $visitor_message, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+     
+} else {
+    echo '<p>Something went wrong</p>';
+}
+ 
+?>
+

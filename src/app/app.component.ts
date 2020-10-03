a72f5f3b122721fd9ea9cc6cc6ddcd3d759f50d9
Ravindu Sachintha <ravindusachintha53@gmail.com>
2020-10-04 00:08:03 +0530

    scrollspy functionality implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentSection = 'section1';
+
+  onSectionChange(sectionId: string) {
+    this.currentSection = sectionId;
+  }
+
+  scrollTo(section) {
+    document.querySelector('#' + section).scrollIntoView({
+      behavior: "smooth", block: "start", inline: "nearest"
+    });
+  }
+
+  navItemStyles(section: string) {
+    return this.currentSection === section ? "nav-item active" : "nav-item";
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,26 @@ import { AppComponent } from './app.component';
 import { AlgoHomeComponent } from './views/algo/algo-home/algo-home.component';
 import { HomeComponent } from './views/home/home.component';
 import { MarkdownModule } from 'ngx-markdown';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { SecurityContext } from '@angular/core';
 import { AlgoContributionComponent } from './views/algo/algo-contribution/algo-contribution.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ScrollSpyDirective } from './directives/scroll-spy.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlgoHomeComponent,
     HomeComponent,
-    AlgoContributionComponent
+    AlgoContributionComponent,
+    ScrollSpyDirective
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    MarkdownModule.forRoot({ loader: HttpClient, sanitize: SecurityContext.NONE })
+    MarkdownModule.forRoot({ loader: HttpClient, sanitize: SecurityContext.NONE }),
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

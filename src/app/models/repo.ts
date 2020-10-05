17b248af3aad0a8270812d4f4dc467346f7797f3
Ravindu Sachintha <ravindusachintha53@gmail.com>
2020-10-05 13:43:10 +0530

    repo ordering issue fixed

--- src/app/models/repo.ts
+++ src/app/models/repo.ts
@@ -1,15 +1,39 @@
 export class Repo {
+  id?: number;
+  name?: string;
+  is_private?: boolean;
+  html_url?: string;
+  url?: string;
+  description?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  stargazers_count?: number;
+  forks?: number;
+  open_issues?: number;
+
   constructor(
-    private id?: number,
-    private name?: string,
-    private is_private?: boolean,
-    private html_url?: string,
-    private url?: string,
-    private description?: string,
-    private created_at?: Date,
-    private updated_at?: Date,
-    private stargazers_count?: number,
-    private forks?: number,
-    private open_issues?: number
-  ) {}
+    id?: number,
+    name?: string,
+    is_private?: boolean,
+    html_url?: string,
+    url?: string,
+    description?: string,
+    created_at?: Date,
+    updated_at?: Date,
+    stargazers_count?: number,
+    forks?: number,
+    open_issues?: number
+  ) {
+    this.id = id
+    this.name = name;
+    this.is_private = is_private;
+    this.html_url = html_url;
+    this.url = url;
+    this.description = description;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+    this.stargazers_count = stargazers_count;
+    this.forks = forks;
+    this.open_issues = open_issues;
+  }
 }
--- src/app/services/data-collector.service.ts
+++ src/app/services/data-collector.service.ts
@@ -14,10 +14,23 @@ export class DataCollectorService {
   getOrgRepos(): Observable<Repo[]> {
     let apiUrl = `https://api.github.com/orgs/FOSS-UCSC/repos`;
     return this.http.get(apiUrl).pipe(
-      map((res: any[]) => res.map(
-        (repo: any) => new Repo(repo.id, repo.name, repo.private, repo.html_url, repo.url, repo.description, repo.created_at,
-          repo.updated_at, repo.stargazers_count, repo.forks, repo.open_issues)))
+      map((res: any[]) => {
+        let lst = res.map(
+          (repo: any) => new Repo(repo.id, repo.name, repo.private, repo.html_url, repo.url, repo.description, repo.created_at,
+            repo.updated_at, repo.stargazers_count, repo.forks, repo.open_issues));
+        return lst.sort(this.compareFn);
+      })
     );
   }
+
+  compareFn = (a: Repo, b: Repo) => {
+    let a_count = a.stargazers_count + a.forks;
+    let b_count = b.stargazers_count + b.forks;
+    if (a_count < b_count)
+      return 1;
+    if (a_count >= b_count)
+      return -1;
+    return 0;
+  };
 
 }
